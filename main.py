f3572f796603417aadcb94613ee1d694244a7b71
Will Sims <wsims@breezeline.com>
2022-11-28 15:08:22 -0600

    Fixed

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from craigslist import CraigslistHousing
 import yaml
+from pprint import pprint
 
 
 with open('config.yaml', 'r') as f:
     CONFIG = yaml.safe_load(f)
 
 
-cl_h = CraigslistHousing(
+cl_houses = CraigslistHousing(
     site=CONFIG['site'],
-    area=CONFIG['area'],
+    # area=CONFIG['area'],
     category=CONFIG['category'],
     filters=CONFIG['filters']
 )
 
+current_house_postings = cl_houses.get_results(sort_by='newest', geotagged=True)
 
-print(cl_h.get_results_approx_count())
+for house_listing in current_house_postings:
+    pprint(house_listing)
